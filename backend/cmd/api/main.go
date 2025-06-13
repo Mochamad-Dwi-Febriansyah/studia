e3511bca4608107dacdc8f8b2216d51cd6918c8f
Mochamad Dwi Febriansyah <iniakunnyaorangjoos@gmail.com>
2025-06-14 01:10:48 +0700

    feat(journal): implement full crud endpoints for journals

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,17 +1,56 @@
 package main
 
 import (
-	"net/http"
+	"log"
+	"os"
+	"studia/backend/internal/handler/http"
+	"studia/backend/internal/repository"
+	"studia/backend/internal/usecase"
+	"studia/backend/pkg/database" // Menggunakan paket database yang sudah kita buat
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"messsage" : "pong pong",
-		})
-	}) 
-	router.Run()
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+
+func main() { 
+	dbConfig := database.Config{
+		Host:     getEnv("DB_HOST", "localhost"), // <-- Diubah untuk membaca env var
+		User:     getEnv("DB_USER", "root"),
+		Password: getEnv("DB_PASSWORD", "Terserah123."), // <-- Ganti 'secret' dengan password default jika perlu
+		DBName:   getEnv("DB_NAME", "db_studia"),
+		Port:     getEnv("DB_PORT", "3306"),
+	} 
+	db := database.NewMySQLDatabase(dbConfig)
+ 
+	timeoutContext := 2 * time.Second
+
+	jurnalRepo := repository.NewJurnalRepository(db)
+	jurnalUsecase := usecase.NewJurnalUsecase(jurnalRepo, timeoutContext)
+	jurnalHandler := http.NewJurnalHandler(jurnalUsecase)
+ 
+	r := gin.Default()
+	v1 := r.Group("/v1")
+	{
+		jurnalRoutes := v1.Group("/jurnals")
+		{
+			jurnalRoutes.POST("/", jurnalHandler.Create)
+			jurnalRoutes.GET("/", jurnalHandler.FindAll)
+			jurnalRoutes.GET("/:id", jurnalHandler.FindByID)
+			jurnalRoutes.PUT("/:id", jurnalHandler.Update)
+			jurnalRoutes.DELETE("/:id", jurnalHandler.Delete)
+		}
+	}
+ 
+	log.Println("Server akan berjalan di port :8080")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("Gagal menjalankan server: %v", err)
+	}
 }
